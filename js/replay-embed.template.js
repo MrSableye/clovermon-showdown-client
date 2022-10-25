e7d9736a52544cae474a237e87a3e2d2e657fbde
Mr. Sableye <mister.sableye@gmail.com>
2022-10-25 14:38:05 -0500

    Fixes replays (and this time I mean it!)

--- js/replay-embed.template.js
+++ js/replay-embed.template.js
@@ -34,7 +34,7 @@ requireScript('https://play.pokemonshowdown.com/data/items.js?a7');
 requireScript('https://play.pokemonshowdown.com/data/teambuilder-tables.js?a7');
 requireScript('https://play.pokemonshowdown.com/js/battle-tooltips.js?a7');
 requireScript('https://play.pokemonshowdown.com/js/battle.js?a7');
-requireScript('https://play.pokemonshowdown.com/data/battle-icons.jsa7?');
+requireScript('https://play.pokemonshowdown.com/data/battle-icons.js?a7');
 
 var Replays = {
 	battle: null,
