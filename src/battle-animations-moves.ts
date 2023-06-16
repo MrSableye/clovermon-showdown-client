f35ce94c532dd0814419e12c0027b22bcdc9a407
rexcanyon792 <121215169+rexcanyon792@users.noreply.github.com>
2023-06-15 23:54:45 -0500

    changes surging strikes while im at it

--- src/battle-animations-moves.ts
+++ src/battle-animations-moves.ts
@@ -49003,7 +49003,7 @@ BattleMoveAnims['lashout'] = {anim: BattleMoveAnims['nightslash'].anim};
 BattleMoveAnims['steelroller'] = {anim: BattleMoveAnims['steamroller'].anim};
 BattleMoveAnims['shellsidearmphysical'] = {anim: BattleMoveAnims['poisonjab'].anim};
 BattleMoveAnims['shellsidearmspecial'] = {anim: BattleMoveAnims['sludgebomb'].anim};
-BattleMoveAnims['surgingstrikes'] = {anim: BattleMoveAnims['aquajet'].anim};
+BattleMoveAnims['surgingstrikes'] = {anim: BattleMoveAnims['jetpunch'].anim};
 BattleMoveAnims['eeriespell'] = {anim: BattleMoveAnims['psyshock'].anim};
 
 BattleMoveAnims['axekick'] = {anim: BattleMoveAnims['highjumpkick'].anim};
